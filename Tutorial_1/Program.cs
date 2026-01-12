7abf8d77f6b4001230b90b253b5928727e55dad1
fuji005 <fuji090505@gmail.com>
2026-01-12 14:12:39 +0700

    Cau 4 tutorial 1

---
+++ Tutorial_1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace SystemInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- THONG TIN HE THONG ---");
+            // 1. Phien ban OS
+            Console.WriteLine("OS Version: " + Environment.OSVersion.ToString());
+            
+            // 2. Thu muc hien tai
+            Console.WriteLine("Current Directory: " + Environment.CurrentDirectory);
+            
+            // 3. Thoi gian he thong
+            Console.WriteLine("System Time: " + DateTime.Now.ToString());
+            
+            Console.ReadKey();
+        }
+    }
+}
